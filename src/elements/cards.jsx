bc659619bc23896fedfa7f37a39d29fe433246fd
JosephAssefa-tech <yosephassefa85@gmail.com>
2026-03-16 23:26:31 +0300

    button spacing for cards fixed

--- src/elements/cards.jsx
+++ src/elements/cards.jsx
@@ -25,17 +25,21 @@ function Cards({ product }) {
       <h3 className="text-lg font-semibold mb-2">{product.title}</h3>
       <p className="text-gray-600 mb-2">{product.description}</p>
       <p className="text-gray-600 mb-2">{product.price} $</p>
+        <div className="flex justify-center gap-4 p-2">
       <button 
       onClick={() => navigate(`/product/${product.id}`, { state: product })}
-      className="bg-gray-200 hover:bg-blue-600 px-4 py-2 rounded-md">
+      className="bg-[#facc15] hover:bg-blue-600 hover:text-black px-4 py-2 rounded-md transition"
+      
+      >
         View Details
       </button>
 <button
 onClick={() => addToCart(product)}
-  className="bg-gray-200 hover:bg-blue-600 px-4 py-2 rounded-md"
+  className="bg-[#facc15] hover:bg-blue-600 hover:text-black px-4 py-2 rounded-md transition"
 >
   Add to cart
 </button>
+        </div>
     </div>
     </>
     
