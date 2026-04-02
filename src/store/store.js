046eb0ec622bca4ffb4c33e735f1dbd8584981ae
JosephAssefa-tech <yosephassefa85@gmail.com>
2026-04-02 23:24:58 +0300

    issue while fixing store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { create } from "zustand";
 
-export const store = configureStore
-({
-    reducer:{}
-})
+export const useStore = create((set)=>({
+    count:0,
+    increase:()=>set((state)=>({count:state.count+1})),
+    decrease:()=>set((state)=>({count:state.count-1})),
+}));
+
--- src/store/store_tes.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-export const useStore = create((set)=>({
-    count:0,
-    increase:()=>set((state)=>({count:state.count+1})),
-    decrease:()=>set((state)=>({count:state.count-1})),
-}));
-
