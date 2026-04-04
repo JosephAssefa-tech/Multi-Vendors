9a604dfb9cc8fd810e65d24e36ae607971fe391b
JosephAssefa-tech <yosephassefa85@gmail.com>
2026-04-04 23:59:42 +0300

    thunk added

--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -5,20 +5,17 @@ const initialState = {
 }
 
 const cartSlice = createSlice({
-    name: "cart",
-    initialState,   
-    reducers: {
-        addToCart: (state, action) => {
-            const item = action.payload;
-            const existItem = state.cartItems.find(x => x.id === item.id);
-            if (existItem) {
-                state.cartItems = state.cartItems.map(x => x.id === existItem.id ? item : x);   
-            } else {
-                state.cartItems = [...state.cartItems, item];
-            }   
-        },
-        removeFromCart: (state, action) => {
-            state.cartItems = state.cartItems.filter(x => x.id !== action.payload);
-        }
-    }
-})
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      state.items.push(action.payload);
+    },
+    removeFromCart: (state, action) => {
+      state.items = state.items.filter(item => item.id !== action.payload);
+    },
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
