90a48d73e5c44618fc3d09a81d45ab2d5cd941d0
JosephAssefa-tech <yosephassefa85@gmail.com>
2026-04-04 23:56:24 +0300

    working on RTK

---
+++ src/slices/cartSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    cartItems: []
+}
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState,   
+    reducers: {
+        addToCart: (state, action) => {
+            const item = action.payload;
+            const existItem = state.cartItems.find(x => x.id === item.id);
+            if (existItem) {
+                state.cartItems = state.cartItems.map(x => x.id === existItem.id ? item : x);   
+            } else {
+                state.cartItems = [...state.cartItems, item];
+            }   
+        },
+        removeFromCart: (state, action) => {
+            state.cartItems = state.cartItems.filter(x => x.id !== action.payload);
+        }
+    }
+})
