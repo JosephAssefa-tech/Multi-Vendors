7459d3b91e888f242f599fb5dfc9acf40c368e6e
JosephAssefa-tech <yosephassefa85@gmail.com>
2026-06-02 00:00:32 +0300

    init

--- my-react-app/src/components/Footer/Footer.jsx
+++ my-react-app/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ function Footer()
     return(<>
     <footer className="bg-gray-800 text-white py-6">
       <div className="container mx-auto px-4">
-        <p>&copy; 2026 My Store. All rights reserved.</p>
+        <p>&copy; 2026 My Store. All rights reserved...</p>
       </div>
     </footer>
     </>)
